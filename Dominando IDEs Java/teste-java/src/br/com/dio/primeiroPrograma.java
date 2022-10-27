8cc6a51f04cf1a552923019f52f8089566ac247e
RubresonFarias <rubresonfm.1991@outlook.com>
2022-10-27 13:22:19 -0400

    Criação de projeto teste de java em uma IDE, com base no curso Dominando IDEs Java

--- Dominando IDEs Java/teste-java/src/br/com/dio/primeiroPrograma.java
+++ Dominando IDEs Java/teste-java/src/br/com/dio/primeiroPrograma.java
@@ -1,2 +1,21 @@
-package br.com.dio;public class primeiroPrograma {
+package br.com.dio;
+
+import java.util.Scanner;
+
+public class primeiroPrograma {
+
+    public static void main(String[] args) {
+
+        Scanner entraDado = new Scanner(System.in);
+
+        String nome;
+
+        System.out.println("Informe o seu nome");
+        nome = entraDado.nextLine();
+
+        System.out.println("Olá " + nome + ", o mais novo DEV!");
+        System.out.println("Bem-vindo ao mundo T.I");
+
+    }
+
 }
