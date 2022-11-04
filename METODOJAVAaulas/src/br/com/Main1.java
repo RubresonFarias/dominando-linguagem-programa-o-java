19e0a8757d59c3d160c68a5a7d60c0278e4554fe
RubresonFarias <rubresonfm.1991@outlook.com>
2022-11-04 10:35:21 -0400

    sobrecarga

--- METODOJAVAaulas/src/br/com/Main1.java
+++ METODOJAVAaulas/src/br/com/Main1.java
@@ -2,7 +2,7 @@ package br.com;
 
 import java.util.Scanner;
 
-public class AtividadeMetodos {
+public class Main1 {
 
     public static void main(String[] args) {
 
---
+++ METODOJAVAaulas/src/br/com/Main2.java
@@ -0,0 +1,15 @@
+package br.com;
+
+public class Main2 {
+
+    public static void main(String[] args) {
+
+        System.out.println("Exercício Quadrilátero");
+
+        Quadrilatero.area(3);
+        Quadrilatero.area(5,5);
+        Quadrilatero.area(7,8,9);
+
+    }
+
+}
