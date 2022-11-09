102bc7d9ebb426962926109e88d251cf5d99ec9d
RubresonFarias <rubresonfm.1991@outlook.com>
2022-11-09 09:43:01 -0400

    Estruturas condicionais

--- METODOJAVAaulas/src/br/com/Main2.java
+++ METODOJAVAaulas/src/br/com/Main2.java
@@ -6,10 +6,17 @@ public class Main2 {
 
         System.out.println("Exercício Quadrilátero");
 
-        Quadrilatero.area(3);
-        Quadrilatero.area(5d,5d);
-        Quadrilatero.area(7,8,9);
-        Quadrilatero.area(10f,10f);
+        double areaQuadrado = Quadrilatero.area(3);
+        System.out.println("A área do quadrado é " + areaQuadrado);
+
+        double areaRetangulo = Quadrilatero.area(5d,5d);
+        System.out.println("A área do retângulo é " + areaRetangulo);
+
+        double areaTrapezio = Quadrilatero.area(7,8,9);
+        System.out.println("A área do trapézio é " + areaTrapezio);
+
+        float areaLosango = Quadrilatero.area(10f,10f);
+        System.out.println("A área do losango é " + areaLosango);
 
     }
 
