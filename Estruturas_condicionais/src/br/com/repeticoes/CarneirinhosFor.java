0df3e715289ffe84de07481edfdc3ef6023d89ca
RubresonFarias <rubresonfm.1991@outlook.com>
2022-11-14 17:07:16 -0400

    continuação repetições

---
+++ Estruturas_condicionais/src/br/com/repeticoes/CarneirinhosFor.java
@@ -0,0 +1,13 @@
+package br.com.repeticoes;
+
+public class CarneirinhosFor {
+    
+    public static void main(String[] args) {
+        
+        for(int carneirinhos = 1 ; carneirinhos <=20; carneirinhos ++) {
+			System.out.println(carneirinhos + " - Carneirinho(s)");
+		}
+
+    }
+
+}
