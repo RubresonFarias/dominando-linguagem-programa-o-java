edbd5d7d0af76570746e07f54f9b2eaafe5da0ea
RubresonFarias <rubresonfm.1991@outlook.com>
2022-11-15 18:50:21 -0400

    tratando exceceções

---
+++ Estruturas_condicionais/src/br/com/excecoes/CepInvalidoException.java
@@ -0,0 +1,5 @@
+package br.com.excecoes;
+
+public class CepInvalidoException extends Exception {
+    
+}
---
+++ Estruturas_condicionais/src/br/com/excecoes/FormatarCep.java
@@ -0,0 +1,21 @@
+package br.com.excecoes;
+
+public class FormatarCep {
+    public static void main(String[] args) {
+        try {
+            String cepFormatado = formatarCep("23765064");
+            System.out.println(cepFormatado);
+        } catch (CepInvalidoException e) {
+            //e.printStackTrace();
+            System.out.println("O CEP não conrresponde!");
+        }
+    }
+
+    static String formatarCep(String cep) throws CepInvalidoException{
+        if(cep.length() != 8)
+          throw new CepInvalidoException();
+        
+          //simulando um cep formatado
+          return "23.765-064";
+    }
+}
